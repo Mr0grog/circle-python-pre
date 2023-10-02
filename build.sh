873dcabd332dbb119ed18f96b94d8c9a79f67166
Rob Brackett <rob@robbrackett.com>
2023-10-02 10:39:07 -0700

    Do sudo hack after setting up context

--- build.sh
+++ build.sh
@@ -15,8 +15,15 @@ if [ "${1}" = 'push' ]; then
 fi
 
 docker context create circle || true
+docker context use circle
 docker buildx create --name circle-builder circle || true
 docker buildx use circle-builder
+
+# Enable `sudo` to work inside a multi-architecture Docker build. See:
+#   https://github.com/docker/buildx/issues/1335
+#   https://github.com/multiarch/alpine/issues/32#issuecomment-604521491
+#   https://github.com/multiarch/qemu-user-static/issues/17
+docker run --rm --privileged multiarch/qemu-user-static:register --reset --credential yes
 
 docker buildx build \
     $platform_and_push \
