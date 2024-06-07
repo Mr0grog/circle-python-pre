c1a86fb9d91259b95050d4ee24e1be741d5d2980
Rob Brackett <rob@robbrackett.com>
2024-06-06 20:35:19 -0700

    Support Python 3.13.0b2

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-export PYTHON_VERSION='3.13.0b1'
+export PYTHON_VERSION='3.13.0b2'
 export IMAGE_NAME='mr0grog/circle-python-pre'
 
 echo "=== Building Image for Python ${PYTHON_VERSION} ==="
