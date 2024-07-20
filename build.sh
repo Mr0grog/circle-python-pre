6ae918fb578abff3644990d45c7d95373c36c25d
Rob Brackett <rob@robbrackett.com>
2024-07-20 13:27:57 -0700

    Build Python 3.13.0b4

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-export PYTHON_VERSION='3.13.0b3'
+export PYTHON_VERSION='3.13.0b4'
 export IMAGE_NAME='mr0grog/circle-python-pre'
 
 echo "=== Building Image for Python ${PYTHON_VERSION} ==="
